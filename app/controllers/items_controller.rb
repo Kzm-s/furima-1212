df6c57c3ffedd33e21c32b52246ae0c373819c36
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-19 18:13:48 +0900

    edit,updateアクションのルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, except: :index
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_item, only: [:show, :edit, :update]
 
     def index
         @user = User.all
         @items = Item.all.order(created_at: :desc)
     end
 
-    def show
-        @item = Item.find(params[:id])
-    end
-
     def new
         @item = Item.new
+    end
+
+    def show
     end
 
     def create
@@ -24,9 +24,24 @@ class ItemsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @item.update(item_params)
+            redirect_to item_path
+        else
+            render :edit
+        end
+    end
+
     private
     def item_params
         params.require(:item).permit(:name, :description, :category_id, :status_id, :burde_id, :area_id, :day_id, :price, :image
         ).merge(user_id: current_user.id)
+    end
+
+    def set_item
+        @item = Item.find(params[:id])
     end
 end
