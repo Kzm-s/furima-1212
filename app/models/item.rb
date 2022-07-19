5e1d8989c2b1855eabb5e90a57c13b4da670c606
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-19 12:33:00 +0900

    ActiveHashの導入２

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :user
+    has_one_attached :image
+    belongs_to_active_hash :category
+    belongs_to_active_hash :status
+    belongs_to_active_hash :burde
+    belongs_to_active_hash :area
+    belongs_to_active_hash :day
 end
