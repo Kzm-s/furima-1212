06dfb88f9e2bf83489c382600795c077b3065ebb
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-19 13:34:35 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, except: :index
-    
+
     def index
         @user = User.all
         @items = Item.all
@@ -12,11 +12,17 @@ class ItemsController < ApplicationController
 
     def create
         @item = Item.new(item_params)
+        if @item.valid?
+            @item.save
+            redirect_to root_path
+        else
+            render :new
+        end
     end
 
     private
     def item_params
         params.require(:item).permit(:name, :description, :category_id, :status_id, :burde_id, :area_id, :day_id, :price, :image
         ).merge(user_id: current_user.id)
-        end
+    end
 end
