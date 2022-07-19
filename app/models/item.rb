06e732612eddc9aede8907de8cf2b2cde2879605
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-19 12:46:43 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,16 @@ class Item < ApplicationRecord
     belongs_to_active_hash :burde
     belongs_to_active_hash :area
     belongs_to_active_hash :day
+
+    with_options presence: true do
+        validates :name, :description, :image
+
+        with_options numericality: { other_than: 1 } do
+            validates :category_id, :status_id, :burde_id, :area_id, :day_id
+        end
+
+        with_options format: { with: /\A[0-9]+\z/ } do
+            validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+        end
+    end
 end
