d6886b47a3d6732cbb1c89ce9d841cf4dd5dfaaf
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-19 11:40:14 +0900

    new,createアクションのルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,18 @@ class ItemsController < ApplicationController
         @user = User.all
         @items = Item.all
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :description, :category_id, :status_id, :burde_id, :area_id, :day_id, :price, :image
+        ).merge(user_id: current_user.id)
+        end
 end
