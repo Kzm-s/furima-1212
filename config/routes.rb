bb501b322f536f6b2438006cc6bf02955e14b1d8
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-20 11:51:24 +0900

    購入画面表示のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :items do
+    resources :orders, only: [:index]
+  end
 end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
