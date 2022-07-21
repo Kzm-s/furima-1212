13e7eb67ee68eb2286228d9073d15f486fe14f23
Kzm-s <amuzak.ikasas@gmail.com>
2022-07-21 13:18:21 +0900

    条件分岐

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,11 @@
 class OrderAddress
 
     include ActiveModel::Model
-    attr_accessor :user_id, :item_id, :order_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
+    attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
     with_options presence: true do
         validates :user_id
         validates :item_id
-        validates :order_id
         validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
         validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
         validates :city
